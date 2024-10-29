826a2ab97378f2f146d10205c9ec222a465e4aae
Ethan Trout <ethan-james-trout@outlook.com>
2024-10-29 15:01:49 +0000

    feat: added workouts app and models. workout, fitness, sport and level

---
+++ workouts/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Workout, Level, Fitness, Sport
+
+# Register your models here.
+admin.site.register(Workout)
+admin.site.register(Level)
+admin.site.register(Fitness)
+admin.site.register(Sport)
+
---
+++ workouts/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+# Create your models here.
+
+class Fitness(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Sport(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Level(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Workout(models.Model):
+    fitness = models.ForeignKey('Fitness', null=True, blank=True, on_delete=models.SET_NULL)
+    sport = models.ForeignKey('Sport', null=True, blank=True, on_delete=models.SET_NULL)
+    level = models.ForeignKey('Level', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+

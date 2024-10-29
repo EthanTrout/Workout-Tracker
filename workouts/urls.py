51959074125b98be641481939e1a782e3601cf06
Ethan Trout <ethan-james-trout@outlook.com>
2024-10-29 19:06:59 +0000

    feat: added search functionality to workouts. fix: fixed reverse lookup error by correcting all_workouts name

--- workouts/urls.py
+++ workouts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_workouts, name = 'all_workouts'),
+    path('', views.all_workouts, name = 'workouts'),
     path('<workout_id>', views.workout_details, name = 'workout_details'),
     
 ]
--- workouts/views.py
+++ workouts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages 
+from django.db.models import Q
 from .models import Workout
 
 # Create your views here.
@@ -7,9 +9,20 @@ def all_workouts(request):
     """ View to show all workouts """
 
     workouts = Workout.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request,"you didn't enter any search criteria!")
+                return redirect(reverse('workouts'))
+            queries =   Q(name__icontains=query) | Q(fitness__name__icontains=query) | Q(sport__name__icontains=query) | Q(level__name__icontains=query) | Q(description__icontains=query)
+            workouts = workouts.filter(queries)
 
     context = {
-        'workouts':workouts
+        'workouts':workouts,
+        'search_query':query
     }
 
     return render(request,'workouts/workouts.html',context)

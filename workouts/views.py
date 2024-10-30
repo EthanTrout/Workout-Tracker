de5f859a6fe657e1a053dd5f02188af279bae4fe
Ethan Trout <ethan-james-trout@outlook.com>
2024-10-30 13:01:43 +0000

    feat: added fitness,sport and level filtering for all workouts

--- workouts/views.py
+++ workouts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages 
 from django.db.models import Q
-from .models import Workout
+from .models import Workout, Fitness, Sport, Level
 
 # Create your views here.
 
@@ -10,8 +10,27 @@ def all_workouts(request):
 
     workouts = Workout.objects.all()
     query = None
+    sports = None
+    fitnesses = None
+    levels = None
 
     if request.GET:
+
+        if 'fitness' in request.GET:
+            fitnesses = request.GET['fitness'].split(',')
+            workouts = workouts.filter(fitness__name__in=fitnesses)
+            fitnesses = Fitness.objects.filter(name__in=fitnesses)
+
+        if 'sport' in request.GET:
+            sports = request.GET['sport'].split(',')
+            workouts = workouts.filter(sport__name__in=sports)
+            sports = Sport.objects.filter(name__in=sports)
+
+        if 'level' in request.GET:
+            levels = request.GET['level'].split(',')
+            workouts = workouts.filter(level__name__in=levels)
+            levels = Level.objects.filter(name__in=levels)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -22,7 +41,10 @@ def all_workouts(request):
 
     context = {
         'workouts':workouts,
-        'search_query':query
+        'search_query':query,
+        'current_sport':sports,
+        'current_fitness':fitnesses,
+        'current_level':levels,
     }
 
     return render(request,'workouts/workouts.html',context)

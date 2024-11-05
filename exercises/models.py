71f1b9fd55e44c5fc4aa62cf196fde1660278f9e
Ethan Trout <ethan-james-trout@outlook.com>
2024-11-05 18:22:14 +0000

    feat: added bodypart model to exercises. added additional infomation fields and many to many field relating exercises with bodyparts to exercises

--- exercises/models.py
+++ exercises/models.py
@@ -3,12 +3,27 @@ from django.db import models
 
 # Create your models here.
 
+class Bodypart(models.Model):
+    name = models.CharField(max_length=254)
+    similar_bodyparts = models.ManyToManyField('self', blank=True, symmetrical=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Exercise(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
+    starting_position = models.TextField(default='')
+    execution = models.TextField(default='')
+    main_muscles = models.ManyToManyField(
+        Bodypart, blank=True, related_name='primary_exercises'
+    )
+    secondary_muscles = models.ManyToManyField(
+        Bodypart, blank=True, related_name='secondary_exercises'
+    )
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
-

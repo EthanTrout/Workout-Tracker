9a6898f8430e1f83f2ae73107707c02cebdf6f4b
Ethan Trout <ethan-james-trout@outlook.com>
2024-11-07 11:48:51 +0000

    feat: added custom search to view_exercises

--- exercises/urls.py
+++ exercises/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_exercises, name = 'exercises'),
-    path('home', views.exercise_home, name = 'home'),
+    path('home', views.exercise_home, name = 'exercises_home'),
     path('add_exercise/<exercise_id>/', views.add_to_workout, name = 'add_to_workout'),
     path('view', views.view_exercises, name = 'view_exercises'),
     path('exercise_details/<exercise_id>/', views.exercise_details, name = 'exercise_details'),

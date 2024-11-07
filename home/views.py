878fc020c0e0de8b9fa22172111017bd5d43b8b4
Ethan Trout <ethan-james-trout@outlook.com>
2024-11-07 13:31:45 +0000

    feat: added carousel to homapage to display exercises

--- home/views.py
+++ home/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-
+from exercises.models import Exercise
+from itertools import zip_longest
 # Create your views here.
+
+def group_list(lst, n):
+    """Split a list into chunks of size n."""
+    return [lst[i:i + n] for i in range(0, len(lst), n)]
 
 def index(request):
     """ View to return Index Page """
-    return render(request,'home/index.html')
+
+    exercises = Exercise.objects.all()
+    grouped_exercises = group_list(exercises, 4)
+    context ={
+        'exercises':exercises,
+        'grouped_exercises':grouped_exercises
+    }
+    return render(request,'home/index.html',context)
+

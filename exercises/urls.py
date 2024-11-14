b1a340c3ab2b6f8d4796b83944aca94c591622d3
Ethan Trout <ethan-james-trout@outlook.com>
2024-11-14 16:32:37 +0000

    fix: removed exercises, add to workout and new workout details views and urls. this has been replaced with the new create_workout view. all create workout functionality moved to workout to allow app to be less reliant on one another

--- exercises/urls.py
+++ exercises/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_exercises, name = 'exercises'),
-    path('home', views.exercise_home, name = 'exercises_home'),
-    path('add_exercise/<exercise_id>/', views.add_to_workout, name = 'add_to_workout'),
+    path('', views.exercise_home, name = 'exercises_home'),
     path('view', views.view_exercises, name = 'view_exercises'),
     path('exercise_details/<exercise_id>/', views.exercise_details, name = 'exercise_details'),
     
--- workouts/urls.py
+++ workouts/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.all_workouts, name = 'workouts'),
     path('home/', views.workouts_home, name = 'workouts_home'),
-    path('new_workout/', views.new_workout_details, name = 'new_workout_details'),
     path('create_workout/', views.create_workout, name = 'create_workout'),
     path('save_workout/<workout_id>/', views.save_workout, name = 'save_workout'),
     path('<int:workout_id>/', views.workout_details, name = 'workout_details'),

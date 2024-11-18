704a2a1c781b9c0bbee5d78f87b3ee2e6854f508
Ethan Trout <ethan-james-trout@outlook.com>
2024-11-18 17:02:02 +0000

    feat: added checkout functionality with payment intent to stripe

--- workout_tracker_subscription/views.py
+++ workout_tracker_subscription/views.py
@@ -18,13 +18,24 @@ def checkout(request,plan_id):
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     plan = get_object_or_404(Plan, id=plan_id)
+    plan_price = plan.price
+    stripe_total = round(plan.price * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency= settings.STRIPE_CURRENCY
+        )
+    
     order_form = OrderForm()
     template = 'checkout/checkout.html'
+
+    if not stripe_public_key:
+        messages.warning(request,"Stripe Public key missing")
     context = {
         'order_form': order_form,
         'plan':plan,
         'stripe_public_key':stripe_public_key,
-        'id_client_secret':'test client secret'
+        'client_secret':intent.client_secret
     }
 
     return render(request,'workout_tracker_subscription/checkout.html',context)

3a1ca721479783b04854e0d4fe4b538211820eaf
Ethan Trout <ethan-james-trout@outlook.com>
2024-11-21 18:03:29 +0000

    feat: added content to profile page --incomplete

--- profiles/views.py
+++ profiles/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render ,get_object_or_404
 from .models import UserProfile
 from workouts.models import Workout
 
+
 # Create your views here.
+
+
 
 def profile(request):
     user_profile = get_object_or_404(UserProfile, user=request.user)
     workouts = Workout.objects.filter(owner=request.user.userprofile)
     context = {
         'profile':user_profile,
-        'workouts':workouts
+        'workouts':workouts,
     }
     return render(request,'profiles/profile.html',context)

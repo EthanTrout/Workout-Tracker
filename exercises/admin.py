f6a1a825c950865dba7b51a1cd15913489b478e4
Ethan Trout <ethan-james-trout@outlook.com>
2024-11-25 13:39:36 +0000

    feat: added exercise images and updated URLS to match

--- exercises/admin.py
+++ exercises/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Exercise,Bodypart
+from django.utils.html import mark_safe  # For rendering HTML in admin
+from .models import Exercise, Bodypart
 
-# Register your models here.
-admin.site.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    # Display fields in the admin list view
+    list_display = ('name', 'image_url_preview', 'description')
+
+    # Add the image preview in the list view based on image_url
+    def image_url_preview(self, obj):
+        if obj.image_url:
+            return mark_safe(f'<img src="{obj.image_url}" width="100" height="100" />')  # Show a small image
+        return 'No Image URL'
+
+    # Make image_url_preview a column in the admin
+    image_url_preview.short_description = 'Image URL Preview'
+
+# Register your models
+admin.site.register(Exercise, ExerciseAdmin)
 admin.site.register(Bodypart)

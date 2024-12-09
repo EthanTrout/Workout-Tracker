40820f87b9530f30e35721d122da69fdf8b6f691
Ethan Trout <ethan-james-trout@outlook.com>
2024-12-09 19:14:10 +0000

    fix: added image field to all bodyparts

--- exercises/models.py
+++ exercises/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Bodypart(models.Model):
     name = models.CharField(max_length=254)
     similar_bodyparts = models.ManyToManyField('self', blank=True, symmetrical=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
